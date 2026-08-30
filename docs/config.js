5a79a4bf1fc27c53aa75176f9ba4fb31e0e467c5
HRmatch <thehorseridermatch@gmail.com>
2026-08-30 20:06:07 -0300

    Connect review app to Google Sheets backend

--- docs/config.js
+++ docs/config.js
@@ -1,4 +1,4 @@
-// Replace the placeholder below with your deployed Google Apps Script Web App URL ending in /exec.
+// Deployed Google Apps Script Web App endpoint.
 window.HR_REVIEW_CONFIG = {
-  APPS_SCRIPT_URL: 'PASTE_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE'
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbw9mxKQ8Ea3ImDtlZjT983HFzGO0hd7E1ILIHi0rurpkb1n3XgiMnZmmex9qXekYi2w/exec'
 };
