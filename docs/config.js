5810955c942d8b27f58f193c67009db5c33ae89b
HRmatch <thehorseridermatch@gmail.com>
2026-08-30 19:54:11 -0300

    Add Google Sheets endpoint config

---
+++ docs/config.js
@@ -0,0 +1,4 @@
+// Replace the placeholder below with your deployed Google Apps Script Web App URL ending in /exec.
+window.HR_REVIEW_CONFIG = {
+  APPS_SCRIPT_URL: 'PASTE_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE'
+};
